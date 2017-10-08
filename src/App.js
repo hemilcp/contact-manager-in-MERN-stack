989f5370aff1cd7d93a90c0740f0223c221c6f2b
Hemil Parmar <hemilcp@gmail.com>
2017-10-08 01:24:29 -0700

    Bind components and app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//      <div>
+//         <h1> Contact Manager</h1>
+//      </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// src/App.js
+
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import ContactListPage from './pages/contact-list-page';
+import ContactFormPage from './pages/contact-form-page';
 
 class App extends Component {
   render() {
     return (
-     <div>
-        <h1> Contact Manager</h1>
-     </div>
+      <Container>
+        <div className="ui two item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">
+            Contacts List
+          </NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">
+            Add Contact
+          </NavLink>
+        </div>
+        <Route exact path="/" component={ContactListPage}/>
+        <Route path="/contacts/new" component={ContactFormPage}/>
+        <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
+      </Container>
     );
   }
 }
